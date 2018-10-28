7fe6c51dbd248aa9cbd06d3934eb719ba1bd61a7
prk <piknamar@fit.cvut.cz>
2018-10-28 08:02:58 +0100

    compression+decompression up2date

--- lib/parsebmp.py
+++ lib/parsebmp.py
@@ -4,6 +4,8 @@
            http://www.dragonwins.com/domains/getteched/bmp/bmpfileformat.htm
        """
 import sys  # argv
+import numpy as np
+import nimfa
 
 
 def read_image(file_path):
@@ -23,8 +25,6 @@ def parse_file_header(image_file):
     bf_size = int.from_bytes(image_file[2:6], byteorder='little')
     bf_off = int.from_bytes(image_file[10:14], byteorder='little')
 
-    print(bf_type, bf_size, bf_off)
-
     return bf_off
 
 
@@ -32,8 +32,6 @@ def parse_image_header(image_file):
     """ Only parses width/height as the rest is irrelevant."""
     bi_width = int.from_bytes(image_file[18:22], byteorder='little')
     bi_height = int.from_bytes(image_file[22:26], byteorder='little')
-
-    print("width:", bi_width, "height:", bi_height)
 
     return bi_width, bi_height
 
@@ -55,32 +53,55 @@ def create_new_image_file(img_matrix, orig_img_file, offset, scanline_len):
        recalculations (e.g. scanline_len)"""
     new_img = []
     new_img.extend(orig_img_file[:offset])
+    img_matrix = img_matrix.tolist()
     null_padding_bytes = scanline_len - len(img_matrix[0])
     null_bytes = []
+
     for i in range(null_padding_bytes):
         null_bytes.extend([0])
 
-    for i in range(len(img_matrix)):
-        new_img.extend(img_matrix[i])
+    for row in img_matrix:
+        new_img.extend(row)
         new_img.extend(null_bytes)
 
     with open("./new_image.bmp", "wb") as fil:
         fil.write(bytes(new_img))
 
 
+def perform_nmf(img_matrix, max_iter, rank):
+    """ Performs the NMF on the image matrix.
+       TODO: pass parametrs for the nmf (rank, nmf algo, ...)"""
+    V = np.matrix(img_matrix, dtype=np.uint8)
+    print("Running NMF. Params: max_iter:", max_iter, "rank:", rank)
+    nmf = nimfa.Nmf(V, max_iter=max_iter, rank=rank, update='euclidean', objective='fro')
+    nmf_fit = nmf()
+    W = nmf_fit.basis()  # np.float64
+    H = nmf_fit.coef()  # np.float64
+    multMatrix = W*H
+    multMatrix = np.rint(multMatrix)  # rounds but keeps np.float64!
+    multMatrix = multMatrix.astype(np.uint8)
+
+    return multMatrix
+
+
 def compress_image(image_file, scanline_len, width, height, offset):
     """ Compresses the image using NMF.
        Parses the image data into a matrix without the null padding (in order
-       to compress less data). TODO what happens next."""
+       to compress less data). NMF compression can be done with various
+       parameters."""
     img_matrix = [[0 for x in range(width)] for y in range(height)]
     orig_offset = offset  # Used in the test function, can be parsed though
 
     for i in range(height):
         # Important to read only the part without the null padding
-        img_matrix[i] = image_file[offset:(offset + width * 3)]
+        # Also assigning list so that the bytes get converted into a list
+        # (easier usage with numpy)
+        img_matrix[i] = list(image_file[offset:(offset + width * 3)])
         offset = offset + scanline_len
 
-    create_new_image_file(img_matrix, image_file, orig_offset, scanline_len)
+    img_matrix_postnmf = perform_nmf(img_matrix, 10, 300)
+
+    create_new_image_file(img_matrix_postnmf, image_file, orig_offset, scanline_len)
 
 
 def main():
